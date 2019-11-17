4dbaaa7ae098ea85d64e54d137b62795d97ed75e
Pierre <riandeypierre@gmail.Com>
2019-11-17 16:51:30 +0100

    cleaned settings init.py

--- tierslieux/settings/__init__.py
+++ tierslieux/settings/__init__.py
@@ -15,21 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cbig5fyn_vzks4t^y_4mwrz!s7spf(7-#bhffdm0e4+)bfgpu*'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,22 +63,6 @@ WSGI_APPLICATION = 'tierslieux.wsgi.application'
 AUTH_USER_MODEL = "user.CustomUser"
 # TODO : Passer par les variables de conf pour importer les modèles entre les différentes applications
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-#TODO : [PROD] Isolate prod credentials settings
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tierslieux',
-        'USER': 'tierslieux',
-        'PASSWORD': 'tierslieux',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -115,7 +85,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
